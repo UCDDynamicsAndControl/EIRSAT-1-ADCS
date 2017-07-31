0949f1af866564f0c784a6615b68cfd7d05283e0
vicubso <30343352+vicubso@users.noreply.github.com>
2017-07-31 15:18:17 +0100

    Added different parameters to the three WBC controllers. Organized it even more modular

---
+++ Orbiting_Brick_Model/graphics.m
@@ -0,0 +1,17 @@
+%Graphics
+
+figure;
+subplot(3,1,1)
+plot(time,omega_oi);
+xlabel('time (s)')
+ylabel('\omega_oi (rad/s)')
+
+subplot(3,1,2)
+plot(time,Euler_angl);
+xlabel('time (s)')
+ylabel('Euler angles (rad)')
+
+subplot(3,1,3)
+plot(time,torques);
+xlabel('time (s)')
+ylabel('Torques (Nm)')
