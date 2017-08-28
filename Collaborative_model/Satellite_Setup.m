10c2ba84210f3056e3bacd8779c7b0a6a2b317cf
vicubso <vicubso@gmail.com>
2017-08-28 11:53:27 +0100

    Updating Changes

--- Collaborative_model/Satellite_Setup.m
+++ Collaborative_model/Satellite_Setup.m
@@ -23,15 +23,3 @@ aerodrag_A_d = 0.2*0.1*sqrt(2); % Aerodynamic area.
 omega_orbit=[0;-7.67/(401.1+6371);0];% Ang vel of orbit frame wrt inertial
 % frame. give the same orbit period as ISS, buit is orbitng about equator
 altitude=405000;%similar altitude as ISS;
-
-%% initial state 
-
-wb_bi_init=[0,0,0];%init ang vel of body frame wrt inertial frame
-qb_o_init=angle2quat(0.5,0,0,'XYZ');
-qb_i_init=angle2quat(0,0,0,'XYZ');
-qo_i_init=angle2quat(0,0,0,'XYZ');
-lat_init=0;
-long_init=0;
-
-x_0=transpose([wb_bi_init,qb_i_init]);%inital state vector of satelite 
-x_orbit_init=[long_init,lat_init,altitude,qb_o_init,qo_i_init];%initial orbit parameters
